2a76fd90db234dac42007cb9ef8f39b6b4c120f5
Bexus Soluções Digitais <leandro@bexus.com.br>
2020-11-21 19:05:24 -0300

    Estrutura FrontEnd e Backencd

---
+++ frontend/src/main/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import React from 'react'
+
+import Logo from '../components/template/Logo'
+import Nav from '../components/template/Nav'
+import Main from '../components/template/Main'
+import Footer from '../components/template/Footer'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default props =>
+    <div className="App">
+        <Logo />
+        <Nav />
+        <Main />
+        <Footer />
+    </div>
+
