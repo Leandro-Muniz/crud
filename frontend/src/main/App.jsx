2da7c90f16ddfdc2cdbf630772b8947c72e9673a
Bexus Soluções Digitais <leandro@bexus.com.br>
2020-11-23 01:14:27 -0300

    Conclusão do Projeto

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -2,19 +2,27 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom' 
+
+// Caso o import BrowserRouter der proplema em produção trocar HashRouter ou configurar servidor Apache
+
+import Routes from './Routes'
 
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
-import Home from '../components/home/Home'
 import Footer from '../components/template/Footer'
 
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default props =>
-    <div className="app">
-        <Logo />
-        <Nav />
-        <Home />
-        <Footer />
-    </div>
+    <BrowserRouter>
+        <div className="app">
+            <Logo />
+            <Nav />
+            <Routes />
+            <Footer />
+        </div>
+    </BrowserRouter>
+
+
 
